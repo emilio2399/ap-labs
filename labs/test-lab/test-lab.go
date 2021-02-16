61967f403bd9c18599862c777bf658032245366c
Katacoda Scenario <scenario@katacoda.com>
2021-02-16 17:40:28 +0000

    Solve test-lab
    Signed-off-by: Katacoda Scenario <scenario@katacoda.com>

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+  if (len(os.Args) < 2) {
+  	fmt.Println("Error, no arguments") 
+  	} else {
+  	  i:= 1
+  	  msg:= ""
+  	  
+  	  for i<len(os.Args){
+  	    if i ==1 {
+  	      msg=msg + os.Args[i]
+  	    }else{
+  	      msg=msg + " " + os.Args[i]
+  	    }
+  	    i++;
+  	  }
+  	  
+      fmt.Println("Hello " +msg+ ", Welcome to the jungle")
+  	}
+  	
+  
 }
