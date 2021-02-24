c54a69ea02a8e13c94660632d8119a295feb86c5
emilio2399 <emilio_ca2399@hotmail.com>
2021-02-24 19:43:58 +0000

    Solve word-reverser
    Signed-off-by: emilio2399 <emilio_ca2399@hotmail.com>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
-int main(){
-    // Place your magic here
+#define IN   1   /* inside a word */
+#define OUT  0   /* outside a word */
+
+#define ARRAY_LENGHT 100
+
+
+
+
+
+
+int main()
+
+{
+
+    int pos=0;
+    char c, word[ARRAY_LENGHT];
+    
+
+
+    while ((c = getchar()) != EOF) {  
+	if (c == '\n'){  
+        for(int j=pos; j>=0; j--){
+        printf("%c", word[j]);
+        }
+        //pos=0;
+    }else{
+        word[pos] = c;
+        pos++;
+        }
+    }
+
     return 0;
 }
