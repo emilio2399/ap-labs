9d01f02d7e413762777805c5691e4ff4467f99e5
emilio2399 <emilio_ca2399@hotmail.com>
2021-03-24 00:26:16 +0000

    Solve str-lib

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
-int main() {
-    return 0;
+int mystrlen(char *str);
+
+char *mystradd(char *origin, char *addition);
+
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char **argv) {
+    
+
+	printf("Initial Length: %d\n", mystrlen(argv[1]));
+	printf("New String: %s\n", mystradd(argv[1],argv[2]));
+    if(mystrfind(mystradd(argv[1],argv[2]), argv[3])){
+        printf("Substring was found : yes\n");
+    }else{
+        printf("Substring was found : no\n");
+    }
+	return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,54 @@
-
+#include <stdlib.h>
+#include <stdio.h>
 
 int mystrlen(char *str){
-    return 0;
+    int c = 0;
+    while(str[c]!='\0'){
+        c++;
+    }
+    return c;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int len = mystrlen(origin) + mystrlen(addition);
+    char *ret = malloc(len +1 * sizeof(char));
+    int c = 0;
+    int b = 0;
+    while(origin[c]!='\0'){
+        ret[c]=origin[c];
+        c++;
+    }
+    b = c;
+    c = 0;
+    while(addition[c]!='\0'){
+        ret[b]=addition[c];
+        b++;
+        c++;
+    }
+    ret[b]='\0';
+    return ret;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int c = 0;
+    int b = 0;
+    int ret = 0;
+    int aux = 0;
+    while(origin[c]!='\0'){
+        if(substr[b]==origin[c] && substr[b]!='\0'){
+            b++;
+            c++;
+            aux = c;
+            ret = 1;
+        }else if(substr[b]=='\0' && ret == 1){
+            return ret;
+        }else if(substr[b]!=origin[c] && b>0){
+            ret = 0;
+            b = 0;
+            c=aux;
+        }else{
+            c++;
+        }
+    }
+    return ret;
 }
